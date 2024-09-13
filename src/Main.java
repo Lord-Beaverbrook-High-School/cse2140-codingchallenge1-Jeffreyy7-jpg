d4e3115d416217495b36a91e76bda9969d8baa63
38278012 <jeffreyy7@educbe.ca>
2024-09-13 10:22:32 -0600

    finished

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner myScan = new Scanner(System.in);
-        System.out.println("Emter a 4 digit year")
+        System.out.println("Enter a 4 digit year"); //typo Emter lol (fixed now) :)
         int year = myScan.nextInt();
         /* using MATHEMATICAL operations (no Strings), split a year
         Example: if the user enters 1989, the output would be:
@@ -12,6 +12,51 @@ public class Main {
         8
         9
 
+         */
+        //Code segment (all are similar but classify the last digit of the year to different variables):
+
+        int lastDigit; //declares lastDigit as an integer
+        lastDigit = year % 10; // finds the last digit of the 4 digit number
+        int onesPlace = lastDigit; // declares a variable "onesPlace" and assigns a the value of the lastDigit to it
+        year -= lastDigit;
+        year /= 10; // line and line above subtract the last digit from the 4 digit number and divide by 10 (removes the last digit)
+
+        //code repeats 4 times (but with different variables than "onesPlace")
+
+        lastDigit = year % 10;
+        int tensPlace = lastDigit; //stores the last digit of the modified year in "tensPlace"
+        year -= lastDigit;
+        year /= 10;
+        lastDigit = year % 10;
+        int hundredsPlace = lastDigit; //stores the last digit of the modified year in "hundredsPlace"
+        year -= lastDigit;
+        year /= 10;
+        lastDigit = year % 10;
+        int thousandsPlace = lastDigit; //stores the last digit of the modified year in "thousandsPlace"
+        year -= lastDigit;
+        year /= 10;
+
+        //these next 4 lines print out the different variable values (which are different digits from the variable "year")
+        System.out.println(thousandsPlace);
+        System.out.println(hundredsPlace);
+        System.out.println(tensPlace);
+        System.out.println(onesPlace);
+
+        /*
+         Bad ide- I mean learning opportunities that did not work:
+         System.out.println(year % 10000);
+         System.out.println(year % 1000);
+         System.out.println(year % 100);
+         System.out.println(year % 10);
+
+         output:
+
+         Enter a 4 digit year
+         1934
+         1934
+         934
+         34
+         4
          */
     }
 }
